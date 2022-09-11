06fae39db18b3d5a444472266f8ed0630d9af36f
JyotiPandey111 <jyotipandeydip@gmail.com>
2022-09-11 17:08:56 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
